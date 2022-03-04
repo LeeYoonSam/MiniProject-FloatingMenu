0e51b5f1058fa393d93279173d7328d6849d708b
YS <yoonsam86@gmail.com>
2022-03-04 22:43:27 +0900

    menu 추가

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 ## 2. app/build.gradle 플러그인 추가 및 ContextMenu 만들 준비
 
 ### kotlin-android-extensions 플러그인 추가
-- xml 에 선언된 위젯 아이디를 선언하지 않고 바로 사용할수 있음
+- xml 에 선언된 위젯 아이디를 선언하지 않고 바로 사용할수 있습니다.
 
-
+### 3. menu 추가
+- res 우 클릭 > New > Android Resource File > Resource Type: menu > floating_context_menu 생성
+- `onCreateContextMenu` 를 오버라이딩해서 생성한 메뉴를 inflate 시킵니다.
--- app/src/main/java/com/ys/mini/floatingmenu/MainActivity.kt
+++ app/src/main/java/com/ys/mini/floatingmenu/MainActivity.kt
@@ -2,6 +2,9 @@ package com.ys.mini.floatingmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.btnOpenMenu
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 		registerForContextMenu(btnOpenMenu)
+	}
+
+	override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenuInfo?) {
+		super.onCreateContextMenu(menu, v, menuInfo)
+		menuInflater.inflate(R.menu.floating_context_menu, menu)
 	}
 }
