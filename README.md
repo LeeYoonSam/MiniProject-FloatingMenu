aad7ad626674ea22b21441d2f934f55dab8ab6fe
YS <yoonsam86@gmail.com>
2022-03-04 23:18:18 +0900

    ContextMenu 선택 처리 및 PopupMenu 추가

--- README.md
+++ README.md
@@ -10,3 +10,24 @@
 ### 3. menu 추가
 - res 우 클릭 > New > Android Resource File > Resource Type: menu > floating_context_menu 생성
 - `onCreateContextMenu` 를 오버라이딩해서 생성한 메뉴를 inflate 시킵니다.
+
+### 4. ContextMenu 선택 처리 및 PopupMenu 추가
+- 이러한 메뉴의 목적은 일반적으로 뷰를 누르고 있을 때 여는 것입니다.
+- 버튼을 길게 누르고 있어야 우리가 원하는 ContextMenu 를 볼수 있습니다.
+- 번외로 클릭시는 PopupMenu 를 사용해서 표시하는 방법을 써 봤습니다.
+
+```kotlin
+fun showPopup(v: View) {
+    val popup = PopupMenu(this, v)
+    val inflater: MenuInflater = popup.menuInflater
+    inflater.inflate(R.menu.floating_context_menu, popup.menu)
+    popup.setOnMenuItemClickListener { item ->
+        clickMenuItem(item)
+        false
+    }
+    popup.show()
+}
+```
+
+## 참고
+[공식 문서 - Menus](https://developer.android.com/guide/topics/ui/menus)
--- app/src/main/java/com/ys/mini/floatingmenu/MainActivity.kt
+++ app/src/main/java/com/ys/mini/floatingmenu/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.ContextMenu.ContextMenuInfo
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.btnOpenMenu
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +19,40 @@ class MainActivity : AppCompatActivity() {
 		registerForContextMenu(btnOpenMenu)
 	}
 
+	// 이 뷰에 대한 컨텍스트 메뉴가 빌드될 때 호출됩니다.
 	override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenuInfo?) {
 		super.onCreateContextMenu(menu, v, menuInfo)
 		menuInflater.inflate(R.menu.floating_context_menu, menu)
+	}
+
+	override fun onContextItemSelected(item: MenuItem): Boolean {
+		clickMenuItem(item)
+		return super.onContextItemSelected(item)
+	}
+
+	// 팝업 메뉴 처리
+	fun showPopup(v: View) {
+		val popup = PopupMenu(this, v)
+		val inflater: MenuInflater = popup.menuInflater
+		inflater.inflate(R.menu.floating_context_menu, popup.menu)
+		popup.setOnMenuItemClickListener { item ->
+			clickMenuItem(item)
+			false
+		}
+		popup.show()
+	}
+
+	private fun clickMenuItem(item: MenuItem) {
+		when(item.itemId) {
+			// 같은 작업을 하면 한번에 처리할수 있지만 구분되어 보이게 따로 처리했습니다.
+			R.id.item1 -> showToast(item.title)
+			R.id.item2 -> showToast(item.title)
+			R.id.item3 -> showToast(item.title)
+			R.id.item4 -> showToast(item.title)
+		}
+	}
+
+	private fun showToast(message: CharSequence) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 	}
 }
